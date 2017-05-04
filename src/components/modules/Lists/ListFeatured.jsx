e82f559eb4f6ebf7b04941327404432b7dbb6833
danjonescidtrix <dannyjones360@me.com>
2017-05-04 09:29:16 +0100

    Further styling

--- src/components/modules/Lists/ListFeatured.jsx
+++ src/components/modules/Lists/ListFeatured.jsx
@@ -1,43 +1,46 @@
 import React from "react";
 var classNames = require('classnames');
 import ListItemFeatured from './ListItems/ListItemFeatured.jsx';
-if(process.env.WEBPACK) require('./List.scss');
+if (process.env.WEBPACK)
+  require('./List.scss');
 import {
-	Container,
-	Row,
-	Col,
-	Visible,
-	Hidden,
-	ScreenClassRender
+  Container,
+  Row,
+  Col,
+  Visible,
+  Hidden,
+  ScreenClassRender
 } from 'react-grid-system';
 
 class ListFeatured extends React.Component {
 
-	constructor() {
-		super();
-		//sets initial state
-		this.state = {};
-	}
+  constructor() {
+    super();
+    //sets initial state
+    this.state = {};
+  }
 
-	// ON COMPONENT RENDER
-	componentDidMount = () => {
-		//console.log("Rendered - " + this.constructor.name);
-		//console.log(this.props.articleList);
-		//console.log(this.props);
-	}
-	// ====>>
+  // ON COMPONENT RENDER
+  componentDidMount = () => {
+    //console.log("Rendered - " + this.constructor.name);
+    //console.log(this.props.articleList);
+    //console.log(this.props);
+  }
+  // ====>>
 
-	render() {
-		return (
-			<Row>
-				<Col sm={12}>
-					<div className="List List--FeaturedSquare">
-						{this.props.articleList.map((article, i) => <ListItemFeatured key={i} article={article}/>)}
-					</div>
-				</Col>
-			</Row>
-		);
-	}
+  render() {
+    return (
+      <Row>
+        <Col sm={12}>
+          <Row>
+            <div className="List List--FeaturedSquare">
+              {this.props.articleList.map((article, i) => <ListItemFeatured key={i} article={article}/>)}
+            </div>
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default ListFeatured;
--- src/components/modules/Lists/ListItems/ListItemFeatured.jsx
+++ src/components/modules/Lists/ListItems/ListItemFeatured.jsx
@@ -1,40 +1,43 @@
 import React from "react";
 import {IndexLink, Link} from "react-router";
 var classNames = require('classnames');
-if(process.env.WEBPACK) require('./ListItem.scss');
+if (process.env.WEBPACK)
+  require('./ListItem.scss');
 import {
-    Container,
-    Row,
-    Col,
-    Visible,
-    Hidden,
-    ScreenClassRender
+  Container,
+  Row,
+  Col,
+  Visible,
+  Hidden,
+  ScreenClassRender
 } from 'react-grid-system';
 
 class ListItemFeatured extends React.Component {
 
-    // BEFORE COMPONENT RENDER (For Everyhing else)
-    constructor() {
-        super();
-        //sets initial state
-        this.state = {};
-    }
+  // BEFORE COMPONENT RENDER (For Everyhing else)
+  constructor() {
+    super();
+    //sets initial state
+    this.state = {};
+  }
 
-    // ON COMPONENT RENDER
-    componentDidMount = () => {
-        //console.log("Rendered - " + this.constructor.name);
-        //console.log(this.props);
-    }
-    // ====>>
+  // ON COMPONENT RENDER
+  componentDidMount = () => {
+    //console.log("Rendered - " + this.constructor.name);
+    //console.log(this.props);
+  }
+  // ====>>
 
-    render() {
-        return (
-            <div className="ListItem ListItem--FeaturedSquare fadeInSeq">
-                <Link to={'/article/' + this.props.article._id}>{this.props.article.title}</Link>
-                <p>{this.props.article.subTitle}</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <Col xs={6} sm={3} md={2} className="fadeInSeq">
+          <div className="ListItem ListItem--FeaturedSquare">
+            <Link to={'/article/' + this.props.article._id}>{this.props.article.title}</Link>
+            <p>{this.props.article.subTitle}</p>
+          </div>
+        </Col>
+    );
+  }
 }
 
 export default ListItemFeatured;
