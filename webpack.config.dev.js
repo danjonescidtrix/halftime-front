803cbc73399ae1b3e86bb5b1311c2af4086d6b94
dan jones <dannyjones360@me.com>
2017-05-06 20:00:44 +0100

    added webpack-dashboard to package.json

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
-var DashboardPlugin = require('webpack-dashboard/plugin');
+//var DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = {
 	entry: [
